b6a4ad195ca36482503435ed49122f6c3d623e21
Daisuke Suzuki <bboydaisuke@gmail.com>
2017-10-19 12:22:52 +0900

    initial commit

---
+++ Assets/BulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    [SerializeField]
+    float m_speed = 10f;
+
+    [SerializeField]
+    float m_distance = 10f;
+
+    Vector3 m_startPosition;
+
+    private void Start()
+    {
+        m_startPosition = transform.position;
+    }
+
+    void FixedUpdate () {
+        if (Vector3.Distance(transform.position, m_startPosition) > m_distance) Destroy(gameObject);
+        transform.position = transform.position + transform.up.normalized * m_speed;
+	}
+}
---
+++ Assets/ShootController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_bulletPrefab;
+
+    [SerializeField]
+    Transform m_muzzlePoint;
+
+    [SerializeField]
+    int m_bulletCountInOneShot = 10;
+
+    [SerializeField]
+    float m_shootRadius = 1.0f;
+
+    [SerializeField]
+    int m_shootCountInOneFire = 5;
+
+    [SerializeField]
+    float m_shootingInterval = 0.1f;
+
+    void Fire()
+    {
+        StartCoroutine(FireAsync(m_shootCountInOneFire));
+    }
+
+    IEnumerator FireAsync(int shootCount)
+    {
+        for (int j = 0; j < shootCount; j++)
+        {
+            for (int i = 0; i < m_bulletCountInOneShot; i++)
+            {
+                Vector3 pos = new Vector3(m_shootRadius * Mathf.Sin(Mathf.Deg2Rad * i * 360.0f / (float)m_bulletCountInOneShot),
+                    m_shootRadius * Mathf.Cos(Mathf.Deg2Rad * i * 360.0f / (float)m_bulletCountInOneShot),
+                    0);
+                Instantiate(m_bulletPrefab, pos, m_bulletPrefab.transform.rotation);
+            }
+            yield return new WaitForSeconds(m_shootingInterval);
+        }
+    }
+
+	void Update () {
+		if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Fire1.");
+            Fire();
+        }
+	}
+}
